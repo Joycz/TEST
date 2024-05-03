56fcbd8f684d75b4ba3956fed234b5e99a4d4b2c
Le Phat Dat <tykengaming153@gmail.com>
2024-05-03 22:26:17 +0700

    FIX ALL

--- AVI linux.py
+++ AVI linux.py
@@ -28,7 +28,6 @@ def read_user_agent_from_json():
     return user_agent
 
 def open_browser(num_windows):
-    chromedriver_path = "chromedriver.exe"
     screen_width = 1920
     screen_height = 500
     num_windows = num_windows
@@ -128,48 +127,6 @@ def check_login(driver):
             return
         '''' ---------------------- '''
 
-
-# def ads(driver):
-#     driver.switch_to.window(driver.window_handles[0])
-#     driver.get('https://aviso.bz/work-serf')
-#     nhiem_vu = driver.find_elements(By.XPATH, "(//div[contains(@id, 'start-serf-')]/a)")
-
-#     if not nhiem_vu:
-#         print("Không có nhiệm vụ quảng cáo.")
-#         return
-
-#     for index, nhiemvu in enumerate(nhiem_vu):
-#         try:
-#             nhiemvu.click()
-#             time.sleep(3)
-#             start_yes_serf_element = WebDriverWait(driver, 10).until(
-#                 EC.presence_of_element_located((By.CLASS_NAME, "start-yes-serf"))
-#             )
-#             start_yes_serf_element.click()
-#             time.sleep(3)
-#             driver.switch_to.window(driver.window_handles[-1])
-#             time.sleep(4)
-#             iframe_xpath = "//frameset//frame[1]"
-#             iframe = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, iframe_xpath)))
-#             driver.switch_to.frame(iframe)
-#             timer_element = driver.find_element(By.XPATH, "(//span[@id='timer_inp'])[1]")
-#             timer_value = int(timer_element.text.strip())
-#             time.sleep(timer_value)
-#             time.sleep(3)
-#             button_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn_capt")))
-#             button_element.click()
-#             driver.close()
-#             driver.switch_to.window(driver.window_handles[0])
-#         except TimeoutException:
-#             print("Không tìm thấy phần tử 'start-yes-serf' sau khi click")
-#         except NoSuchElementException:
-#             print("Không tìm thấy phần tử timer hoặc button")
-#         except Exception as e:
-#             print(f"Lỗi khi thực hiện nhiệm vụ quảng cáo: {str(e)}")
-
-#     print("Hết nhiệm vụ quảng cáo")
-
-
 def job_ytb_test(driver):
     nhiem_vu = driver.find_elements(By.CSS_SELECTOR, "[id^='link_ads_start']")
     for index, nhiemvu in enumerate(nhiem_vu):
@@ -241,76 +198,6 @@ def job_ytb_test(driver):
             pass
     pass
 
-def job_ytb(driver):
-    nhiem_vu = driver.find_elements(By.CSS_SELECTOR, "[id^='link_ads_start']")
-    for index, nhiemvu in enumerate(nhiem_vu):
-        try:
-            driver.switch_to.window(driver.window_handles[0])
-            print("Nhiệm vụ thứ :", index, end='\r')
-            nhiemvu.click()
-            driver.switch_to.window(driver.window_handles[1])
-            '''' Thực hiện chờ web load '''
-            try:
-                WebDriverWait(driver, 60).until(
-                    EC.presence_of_element_located((By.TAG_NAME, "body"))
-                )
-                print("Trang load hoàn tất")
-            except:
-                break
-            '''' ---------------------- '''
-            try:
-                time_job = int(driver.find_element(By.ID,'tmr').text)
-                iframe = driver.find_element(By.ID,"video-start") 
-                driver.switch_to.frame(iframe)
-
-                play_button_elements = driver.find_elements(By.CLASS_NAME,"ytp-large-play-button") 
-                found = False
-                for element in play_button_elements: 
-                    classes = element.get_attribute("class") 
-                    if "ytp-large-play-button-red-bg" in classes: 
-                        found = True 
-                        break 
-                if found: #Nếu có nút YT đỏ nằm trong classes.
-                    wait = WebDriverWait(driver, 25)
-                    play_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ytp-large-play-button")))
-                    play_button.click()
-                    time.sleep(time_job + 3)
-                    # Thực hiện thêm 3 giây
-                    try:
-                        driver.switch_to.default_content() #Thoát iframe để lấy time tmr
-                        tmr_element = WebDriverWait(driver, 10).until(
-                            EC.visibility_of_element_located((By.ID, 'tmr'))
-                        )
-                        time_job_2 = int(tmr_element.text)
-
-                        #switch vào iframe lại
-                        iframe = driver.find_element(By.ID, "video-start")
-                        driver.switch_to.frame(iframe)
-
-                        #ấn nút play
-                        play_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ytp-large-play-button")))
-                        play_button.click()
-                        time.sleep(time_job_2 + 3)
-                        driver.close()
-                        driver.switch_to.window(driver.window_handles[0])
-                    except:
-                        driver.close()
-                        driver.switch_to.window(driver.window_handles[0])
-                else:
-                    print("Video không được tìm thấy", end='\r')
-                    driver.close()
-                    driver.switch_to.window(driver.window_handles[0])
-            except:
-                time.sleep(1)
-                driver.close()
-                driver.switch_to.window(driver.window_handles[0])
-        except NoSuchWindowException:
-            print("Không thể chuyển sang tab mới. Lặp lại vòng lặp...")
-            continue
-            
-    # time.sleep(2)
-    # ads(driver)
-
 def main():
     while True:
         try:
@@ -326,10 +213,13 @@ def main():
             driver.quit()
             print("Chờ 5 phút.")
             time.sleep(60 * 5)
-        except:
-            driver.quit()
+        except Exception as e:
+            print("Lỗi:", str(e))
+            if 'driver' in locals():
+                driver.quit()
             time.sleep(60*5)
 
 
 if __name__ == "__main__":
     main()
+
